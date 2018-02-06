36e5f0bee8a79c1aca79f9545cbbd7d6e0a97c15
jd52 <david.stiefel@gmail.com>
2018-02-06 08:04:55 +0300

    third

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-    "net/http"
-    "log"
-    "github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
 )
 
 func YourHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Golang test!\n"))
+	w.Write([]byte("<html><body><h1>Golang test!\n</body></html>"))
 }
 
 func main() {
-    r := mux.NewRouter()
-    // Routes consist of a path and a handler function.
-    r.HandleFunc("/", YourHandler)
+	r := mux.NewRouter()
+	// Routes consist of a path and a handler function.
+	r.HandleFunc("/", YourHandler)
 
-    // Bind to a port and pass our router in
-    log.Fatal(http.ListenAndServe(":8080", r))
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
