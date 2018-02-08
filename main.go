c9ac7f5da5daa5a7e33b5b101447b6d8e3ed3444
jd52 <32954813+jd52@users.noreply.github.com>
2018-02-08 07:20:41 +0300

    Update main.go

--- main.go
+++ main.go
@@ -10,17 +10,10 @@ import (
 )
 
 func YourHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<html><body><h1>Golang test!\n</body></html>"))
-}
-
-
-type Person struct {
-        Name string
-        Phone string
-}
-
-func main() {
-        session, err := mgo.Dial("10.132.0.5")
+	
+	name := mux.Vars(r)
+	
+	session, err := mgo.Dial("10.132.0.5")
         if err != nil {
                 panic(err)
         }
@@ -30,23 +23,34 @@ func main() {
         session.SetMode(mgo.Monotonic, true)
 
         c := session.DB("test").C("people")
-        err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
-	               &Person{"Cla", "+55 53 8402 8510"})
+        err = c.Insert(&Person{"Ale", "+55 53 8116 9439"},
+	               &Person{"Cla", "+55 53 8402 8410"})
         if err != nil {
                 log.Fatal(err)
         }
 
         result := Person{}
-        err = c.Find(bson.M{"name": "Ale"}).One(&result)
+	err = c.Find(bson.M{string(name): "Ale"}).One(&result)
         if err != nil {
                 log.Fatal(err)
         }
 
         fmt.Println("Phone:", result.Phone)
+	w.Write([]byte("<html><body><h1>Golang test!\n"result.Phone</body></html>"))
+}
+
+
+type Person struct {
+        Name string
+        Phone string
+}
+
+func main() {
+
 	
 	r := mux.NewRouter()
 	// Routes consist of a path and a handler function.
-	r.HandleFunc("/", YourHandler)
+	r.HandleFunc("/{name}", YourHandler)
 
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe(":80", r))
