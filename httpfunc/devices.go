9ce892d5692ec6dab46a4f12bd77634cfdce1fd9
robwalker2106 <blkdragon2106@gmail.com>
2018-02-11 18:55:48 +0300

    fmt

--- httpfunc/devices.go
+++ httpfunc/devices.go
@@ -14,8 +14,9 @@ func DeviceHandler(res http.ResponseWriter, req *http.Request) {
 	rh := req.Method
 	if rh == "GET" {
 
-		//deviceList := database.ListDevice(res, req)
-
+		deviceList := database.ListDevice(res, req)
+		fmt.Println(deviceList)
+		fmt.Printf("deviceList is of type %T\n", deviceList)
 		err := tpl.ExecuteTemplate(res, "devices.gohtml", nil)
 		if err != nil {
 			log.Fatalln(err)
