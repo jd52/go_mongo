d57880422df69bf16cf73d8fd5417fb4dc7dc908
jd52 <david.stiefel@gmail.com>
2018-02-11 07:39:13 +0000

    new server change to 8080

--- main.go
+++ main.go
@@ -59,5 +59,5 @@ func main() {
 	r.HandleFunc("/device/{hostname}", YourHandler)
 
 	// Bind to a port and pass our router in
-	log.Fatal(http.ListenAndServe(":80", r))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
