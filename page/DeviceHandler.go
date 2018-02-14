eb1a01386c8053304259f4021666924051f49af7
robwalker2106 <blkdragon2106@gmail.com>
2018-02-14 20:40:40 +0300

    comments

--- page/DeviceHandler.go
+++ page/DeviceHandler.go
@@ -10,7 +10,10 @@ import (
 //This page allows the creation of new items to the MongoDB "Device" Collection.
 func DeviceHandler(res http.ResponseWriter, req *http.Request) {
 	rh := req.Method
+
 	addD := mongo.Device{}
+
+	//Passing the empty addD struct into mongo.ListDevice returns the whole list.
 	deviceList := mongo.ListDevice(&addD, res, req)
 	if rh == "GET" {
 		err := tpl.ExecuteTemplate(res, "devices.gohtml", deviceList)
@@ -23,13 +26,14 @@ func DeviceHandler(res http.ResponseWriter, req *http.Request) {
 		if err != nil {
 			log.Fatalln(err)
 		}
-
+		//Received input from the devices.gohtml template and updates addD.
 		addD = mongo.Device{
 			Hostname:   req.FormValue("hostname"),
 			IPAddress:  req.FormValue("ipAddress"),
 			DeviceType: req.FormValue("deviceType"),
 		}
-
+		//Sends the received input and sends it to mongo.AddDevice to add a new
+		//entry into the mongoDB database.
 		mongo.AddDevice(addD, res, req)
 		deviceList := mongo.ListDevice(&addD, res, req)
 		err = tpl.ExecuteTemplate(res, "devices.gohtml", deviceList)
