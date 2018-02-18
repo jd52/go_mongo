17885f42fdf2d0bb88b644ff875573ab0f156d68
jd52 <david.stiefel@gmail.com>
2018-02-18 09:23:50 +0300

    working on functions to parse CSV and bulk insert into database

---
+++ mongo/ParseCSV.go
@@ -0,0 +1,32 @@
+package mongo
+
+import (
+	"bufio"
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func ParseCSV() {
+	csvFile, _ := os.Open("people.csv")
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+	var devices []Device
+	for {
+		line, error := reader.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
+		}
+		devices = append(devices, Device{
+			Hostname:   line[0],
+			IPAddress:  line[1],
+			DeviceType: line[2],
+		})
+	}
+	devicesJSON, _ := json.Marshal(devices)
+	fmt.Println(string(devicesJSON))
+}
