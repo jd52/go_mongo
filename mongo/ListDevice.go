9c00ca32679c08dfc3863dd1d2859b70ed935b28
jd52 <david.stiefel@gmail.com>
2018-02-18 08:11:16 +0300

    updated "and" find in the listdevice.go file

--- mongo/ListDevice.go
+++ mongo/ListDevice.go
@@ -29,7 +29,7 @@ func ListDevice(qy *Device, andOr *string, w http.ResponseWriter, r *http.Reques
 		err = deviceCollect.Find(bson.M{}).All(&result)
 	} else if *andOr == "and" {
 
-		err = deviceCollect.Find(bson.M{"hostname": qy.Hostname, "ipaddress": qy.IPAddress, "devicetype": qy.DeviceType}).All(&result)
+		err = deviceCollect.Find(bson.M{"$and": []bson.M{bson.M{"hostname": qy.Hostname}, bson.M{"ipaddress": qy.IPAddress}, bson.M{"devicetype": qy.DeviceType}}}).All(&result)
 		fmt.Println(qy)
 	} else {
 
