4840c490de131d58eec7c1485eb7e0f8dbf389b2
jd52 <david.stiefel@gmail.com>
2018-02-18 11:22:42 +0300

    same

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	r.HandleFunc("/index", page.IndexHandler)
 	r.HandleFunc("/devices", page.DeviceHandler)
 	r.HandleFunc("/query", page.QueryHandler)
-	r.HandleFunc("/queryresponse", QueryResponseHandler).Methods("POST")
+	r.HandleFunc("/queryresponse", page.QueryResponseHandler).Methods("POST")
 	//PathPrefix allows local files to be served
 	r.PathPrefix("/css/").Handler(http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
 
