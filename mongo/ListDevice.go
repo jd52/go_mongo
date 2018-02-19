ec5e019aba20d30b836a5c93e02dbf5d72f1a41e
robwalker2106 <blkdragon2106@gmail.com>
2018-02-19 14:53:21 +0300

    all

--- mongo/ListDevice.go
+++ mongo/ListDevice.go
@@ -31,13 +31,12 @@ func ListDevice(qy *Device, andOr *string, w http.ResponseWriter, r *http.Reques
 		fmt.Println(qy)
 	} else {
 
-		err = deviceCollect.Find(bson.M{"hostname": bson.RegEx{Pattern: qy.Hostname, Options: "i"}}).All(&result)
+		err = deviceCollect.Find(bson.M{"$or": []bson.M{bson.M{"hostname": bson.RegEx{Pattern: qy.Hostname, Options: "i"}}, bson.M{"ipaddress": qy.IPAddress}, bson.M{"devicetype": qy.DeviceType}}}).All(&result)
 		fmt.Println(qy)
 	}
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Printing Results: ", result)
-	fmt.Println(bson.M{"hostname": bson.RegEx{Pattern: "^" + qy.Hostname, Options: ""}})
 	return result
 }
