533cc1ffe2457d69e87aee96ea0f3b2098032a0f
byt3_m3 <cbaxter@byt3-mac.local>
2018-02-24 12:44:50 +0300

    Cleaned up logger module

--- logger/Logger.go
+++ logger/Logger.go
@@ -1,7 +1,6 @@
 package logger
 
 import (
-	"fmt"
 	"os"
 )
 
@@ -21,42 +20,101 @@ LogError(params)
 
 //Logger is an stuct to build the logger
 type Logger struct {
-	Err error
-	Lvl string
-	Msg string
+	Err     error
+	Lvl     string
+	Msg     string
+	LogFile string
+	LogDir  string
+	CwDir   string
+	TempDir string
+	FileFQN string
+}
+
+//LogLevel is a struct to refrence Logging levels
+type LogLevel struct {
+	Info    string `json:"info"`
+	Warning string `json:"warning"`
+	Err     string `json:"error"`
+	Panic   string `json:"panic"`
+	Fatal   string `json:"fatal"`
+	Debug   string `json:"debug"`
 }
 
 //writeLog is an internal method of writing received log data
 func (s Logger) writeLog(FILE *os.File, msg *[]byte) error {
-	fmt.Println("It Works")
 	_, err := FILE.Write(*msg)
+	if err != nil {
+		return err
+	}
 	defer FILE.Close()
 	return err
 }
 
-func (s Logger) chkFile(fileName *string) bool {
-	os.Chdir(*fileName)
-	// fmt.Println(*dir)
-	_, statErr := os.Stat(*fileName)
-	if statErr == nil {
-		fmt.Println("chkFile() true")
-		return true
+//chkFile is an internal method that checks the status of a given file,
+//returns bool if a match is encountered
+func (s Logger) chkFile(fileName *string) error {
+	_, statErr := os.Stat("temp/" + *fileName)
+	if statErr != nil {
+		dirErr := os.Chdir(s.FileFQN)
+		if dirErr != nil {
+			return dirErr
+		}
+
+		file, err := os.Create(s.LogFile)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		return err
 
 	}
-
-	fmt.Println("chkFile() false")
-	return false
-}
-
-func (s Logger) getTempdir() string {
-	dir, _ := os.Getwd()
-	dir = (dir + "/tmp")
-	return dir
+	return statErr
 }
 
 func (s Logger) openFile(fileName *string) (os.File, error) {
-	fmt.Println("openFile()")
-	fmt.Println(*fileName)
-	fileResult, fileErr := os.OpenFile(*fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	fileResult, fileErr := os.OpenFile(*fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
 	return *fileResult, fileErr
+}
+
+//SetLogFile is an method that allows the operator to change the default log name
+func (s Logger) SetLogFile(fileName *string) {
+	s.LogFile = *fileName
+}
+
+//SetLogDir is an method that allows the operator to change the default log directory
+func (s Logger) SetLogDir(dir string) {
+	s.LogDir = dir
+}
+
+//GetCwDir is an method that allows the operator to set the current working director
+func (s Logger) GetCwDir() (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	// s.CwDir = dir
+	return dir, err
+}
+
+//GetDir is an method that allows the operator to change the cwd and set the CwDir attribute
+//to the new directory
+func (s Logger) GetDir(dir *string) error {
+	err := os.Chdir(*dir)
+	if err != nil {
+		return err
+	}
+	s.CwDir, err = os.Getwd()
+	return err
+}
+
+//setLogdir is a internal method that sets the TempDir attribute
+func (s Logger) setLogdir() string {
+	TempDir := (s.CwDir + "/" + s.LogDir)
+	return TempDir
+}
+
+func (s Logger) setFilefqn() string {
+	FileFQN := (s.CwDir + "/" + s.LogDir + "/" + s.LogFile)
+	return FileFQN
 }
