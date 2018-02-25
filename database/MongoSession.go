f1818f6b749c463c71d113fbb4370e071bdbf057
robwalker2106 <blkdragon2106@gmail.com>
2018-02-25 12:58:55 +0300

    mgo.Dail

--- database/MongoSession.go
+++ database/MongoSession.go
@@ -9,7 +9,7 @@ import (
 //MongoSession returns a call to a mongoDB
 func MongoSession() *mgo.Session {
 
-	session, err := mgo.Dial("127.0.0.1")
+	session, err := mgo.Dial("10.132.0.5")
 	if err != nil {
 		logger.LogError(&err, "test")
 	}
