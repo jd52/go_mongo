548acf4952f2fe730c49f61c1c96a65bedd31459
byt3-m3 <cbaxtertech@gmail.com>
2018-02-27 11:24:02 +0300

    syntax clean up

--- errorCollector/errRouter.go
+++ errorCollector/errRouter.go
@@ -3,15 +3,14 @@ package errorCollector
 //the Router takes the last entry of the ErrorCollectors error caller and routes based on the condition specefied,
 //Default operation will utilzed the default handler
 func errRouter(e *ErrCollector) {
+	lastErr := e.MyErrs[len(e.MyErrs)-1]
 
-	newErr := e.MyErrs[len(e.MyErrs)-1]
-
-	if newErr.Caller == "main.eee" {
+	if lastErr.Caller == "main.eee" {
 		testHandler(*e)
 		return
 	}
 	//This Routing Entry Needs to be
-	if len(newErr.Caller) > 0 {
+	if len(lastErr.Caller) > 0 {
 		defaultHandler(*e)
 		return
 	}
